bb9014b00cd133a2706f3935843a6c01fcac9908
Rohit Singh <Rohit.Singh@clario.com>
2024-09-04 17:14:56 +0530

    Iterator Pattern Included

---
+++ DesignPatterns/Iterator/BookCollection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LLD.DesignPatterns.Iterator
+{
+    internal class BookCollection : IBookCollection
+    {
+        private readonly List<Book> _books;
+
+        public BookCollection()
+        {
+            _books = new List<Book>();
+        }
+
+        public void Add(Book book) 
+        {
+            _books.Add(book);
+        }   
+
+        public Book this[int index] => _books[index];
+
+        public int Count => _books.Count;
+        public IIterator<Book> CreateIterator()
+        {
+            return new BookIterator(this);
+        }
+    }
+}
---
+++ DesignPatterns/Iterator/BookIterator.cs
@@ -0,0 +1,25 @@
+﻿namespace LLD.DesignPatterns.Iterator
+{
+    internal class BookIterator : IIterator<Book>
+    {
+        private readonly BookCollection _bookCollection;
+        public int currentIndex = -1;
+
+        public BookIterator(BookCollection bookCollection)
+        { 
+            _bookCollection = bookCollection;
+        }
+        public Book Current => _bookCollection[currentIndex];
+
+        public bool MoveNext()
+        {
+            ++currentIndex;
+            return (currentIndex < _bookCollection.Count);
+        }
+
+        public void Reset()
+        {
+            currentIndex = 0;
+        }
+    }
+}
